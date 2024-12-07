25375c970435185e09560473c3d6b1f096bab10d
Olivier St-Amand <olivier.st.amand.1@gmail.com>
2024-12-07 17:05:56 -0500

    Add back base

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
+    base: "/rock-exploding-threejs/",
     plugins: [wasm(), topLevelAwait()],
 });
